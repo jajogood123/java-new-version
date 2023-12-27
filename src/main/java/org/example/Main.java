97618a08b778dd581a94eed360c8e72f8ca49bb7
shaozhenya <shaozhenya@B-HJMLVWV5-1939.local>
2023-12-27 15:43:38 +0800

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author zhenya.szy@alibaba-inc.com
+ * @date 2023/12/27
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
